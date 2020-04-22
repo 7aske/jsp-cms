3259dc48fadcc189b3d4e870020c0fa317833246
Nikola Tasic <ntasic7@gmail.com>
2020-04-22 18:48:00 +0200

    initial

---
+++ src/main/java/com/example/cms/servlet/admin/post/delete/DeletePostServlet.java
@@ -0,0 +1,29 @@
+package com.example.cms.servlet.admin.post.delete;
+
+import com.example.cms.database.dao.BlogPostDAO;
+import com.example.cms.util.UrlUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/admin/post/delete/*")
+public class DeletePostServlet extends HttpServlet {
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String idBlogPostString = UrlUtil.getUrlBase(request.getRequestURL().toString());
+		long idBlogPost;
+		BlogPostDAO blogPostDAO = new BlogPostDAO();
+		System.out.println(idBlogPostString);
+		try {
+			idBlogPost = Long.parseLong(idBlogPostString);
+			blogPostDAO.removeById(idBlogPost);
+		} catch (NumberFormatException ex) {
+			ex.printStackTrace();
+		}
+		response.sendRedirect(request.getContextPath() + "/admin/admin.jsp");
+	}
+}
---
+++ src/main/java/com/example/cms/util/UrlUtil.java
@@ -0,0 +1,8 @@
+package com.example.cms.util;
+
+public class UrlUtil {
+	public static String getUrlBase(String url){
+		String[] parts = url.split("/");
+		return parts[parts.length - 1];
+	}
+}
